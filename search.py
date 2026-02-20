92ad3b27949c0e841153699f0d342979e56133f5
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2026-02-19 20:05:37 -0500

    Question7Setup
    The initial setup for the question 7 problem, lwk just copied over the code from the super class and setup the constructor for now

--- search.py
+++ search.py
@@ -292,7 +292,46 @@ def astar_search(problem):
 	
 	return None, nodes_visited
 #______________________________________________________________________________
-#7 
+#
+#q7 
+class EightPuzzleProblem(Problem):
+	"""The abstract class for a formal problem.  You should subclass this and
+	implement the method successor, and possibly __init__, goal_test, and
+	path_cost. Then you will create instances of your subclass and solve them
+	with the various search functions."""
+	
+	
+
+	def __init__(self, initial, goal=None):
+		super().__init__(initial, "123456780")
+		
+	def successor(self, state):
+		"""Given a state, return a sequence of (action, state) pairs reachable
+		from this state. If there are many successors, consider an iterator
+		that yields the successors one at a time, rather than building them
+		all at once. Iterators will work fine within the framework."""
+		raise NotImplementedError("successor() must be implemented in subclass")
+	
+	##UNNEEDED CUZ WE JUST NEED THEM TO DO WHAT THEY ALREADY DO
+	# def goal_test(self, state):
+	# 	"""Return True if the state is a goal. The default method compares the
+	# 	state to self.goal, as specified in the constructor. Implement this
+	# 	method if checking against a single self.goal is not enough."""
+	# 	return state == self.goal
+	
+	# def path_cost(self, c, state1, action, state2):
+	# 	"""Return the cost of a solution path that arrives at state2 from
+	# 	state1 via action, assuming cost c to get up to state1. If the problem
+	# 	is such that the path doesn't matter, this function will only look at
+	# 	state2.  If the path does matter, it will consider c and maybe state1
+	# 	and action. The default method costs 1 for every step in the path."""
+	# 	return c + 1
+		
+	def h(self, node):
+		"""Return the heuristic function value for a particular node. Implement
+		this if using informed (heuristic) search."""
+		return 0
+#_________________________________________________________________________________
 
 ## Main
 
